7c93d4dcb9bad28abd8c7a07ac8f28d0bace4c94
taira-kobayashi <161178572+taira-kobayashi@users.noreply.github.com>
2024-03-26 18:33:04 +0900

    Update prototypes_controller.rb

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -58,8 +58,7 @@ class PrototypesController < ApplicationController
   end
 
     def move_to_index
-      unless user_signed_in?
-        redirect_to action: :index
-      end
+      redirect_to root_path 
+      unless current_user == @prototype.user
     end
 end
